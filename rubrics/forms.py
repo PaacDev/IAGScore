f048377b8faf8e8fa832a3ce2769c9e47e16f7ac
pac1006 <117029295+pac1006@users.noreply.github.com>
2025-04-01 17:35:39 +0200

    umpdate css form

--- rubrics/forms.py
+++ rubrics/forms.py
@@ -39,7 +39,7 @@ class RubricForm(ModelForm):
         widget=forms.TextInput(
             attrs={
                 "id": "Nombre",
-                "class": "input-custom",
+                "class": "block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-gray-600 sm:text-sm/6",
             }
         ),
         required=True,
