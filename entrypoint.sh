0777066dd2c5c2a04aceb75e026ad41a84506abe
pac1006 <117029295+pac1006@users.noreply.github.com>
2025-05-26 12:10:29 +0200

    Internacionalización #47

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# Ejecutar migraciones
-python manage.py makemigrations
-python manage.py migrate --noinput
+if [[ "$@" == *"celery"* ]]; then
+    echo "Entrypoint detectó ejecución de Celery. Saltando setup de Django."
 
+else
+
+    # Ejecutar migraciones
+    echo "Ejecutandp migraciones..."
+    python manage.py makemigrations
+    python manage.py migrate --noinput
+
+    echo "Compilando archivos de traducción..."
+    python manage.py compilemessages  > /dev/null
+    echo "Compilación de traducción completada."
+
+fi
 # Iniciar el servidor de Django
 exec "$@"
